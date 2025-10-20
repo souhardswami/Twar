3796750aee6ca8ecd0912cc2d776d978857c0da2
souhard <souhardswami987@gmail.com>
2025-10-20 13:04:45 +0530

    Added Db session for all the request

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from db import db
 from config import Config
 from routes.auth_routes import auth_bp
 from routes.account_routes import account_bp
@@ -9,11 +10,13 @@ from routes.flow_routes import flow_bp
 from routes.rag_routes import rag_bp
 from routes.twitter_routes import twitter_bp
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     CORS(app)
     JWTManager(app)
+    db.init_app(app)
     
     app.register_blueprint(account_bp, url_prefix="/account")
     app.register_blueprint(auth_bp, url_prefix="/auth")
--- backend/config.py
+++ backend/config.py
@@ -1,41 +1,18 @@
 import os
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-DATA_FILE = os.path.join(script_dir, 'accounts.json')
-APP_URL = os.getenv('APP_URL')
-CONSUMER_KEY = os.getenv('CONSUMER_KEY')
-CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
-ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
-ACCESS_SECRET = os.getenv('ACCESS_SECRET')
-BEARER_TOKEN = os.getenv('BEARER_TOKEN')
-KEYWORDS = ','.join(['#web3gaming',
-                '#cryptogaming',
-                '#web3game',
-                '#solana',
-                '#solanacoin',
-                '#cryptoproject',
-                '#altcoin',
-                '#nftgaming'])
-PROMPT = 'Generate a friendly and engaging reply to the following tweet promoting our company subtly:'
-
-request_token_url = "https://api.twitter.com/oauth/request_token"
-authorization_url = "https://api.twitter.com/oauth/authorize"
-access_token_url = "https://api.twitter.com/oauth/access_token"
-
-DATA_SCHEMA = {
-    'screen_name' : '',
-    'oauth_token' : '',
-    'oauth_token_secret' : '',
-    'id' : '',
-    'status' : 'active',
-    'keywords' : KEYWORDS,
-    'prompt' : PROMPT,
-    'kpi': {
-        'weekly': '70',
-        'daily': '10'
-    },
-    'left_kpi': {
-        'weekly': '70',
-        'daily': '10'
-    }
-}
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "YOUR_SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "JWT_SECRET")
+    UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "./uploads")
+    DEBUG = os.getenv("DEBUG", True)
+    
+    # For DB 
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql+pymysql://root:mysql@0.0.0.0:3307/Twar_read_db"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # Twitter Related 
+    # request_token_url = "https://api.twitter.com/oauth/request_token"
+    # authorization_url = "https://api.twitter.com/oauth/authorize"
+    # access_token_url = "https://api.twitter.com/oauth/access_token"
