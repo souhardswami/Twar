c80f7a74b6ddfe26f12b568df8f259ae2f18dad2
souhard <souhardswami987@gmail.com>
2025-10-21 14:37:31 +0530

    implemented hashtag agent

--- backend/agents/hashtag_agent.py
+++ backend/agents/hashtag_agent.py
@@ -1,8 +1,15 @@
 from .base_agent import BaseAgent
+from services.flow_service import get_node_details
 
 class HashtagAgent(BaseAgent):
     def run(self, input_data, memory):
-        # hashtags = input_data.split(",")
-        # print(f"[{self.name}] Using hashtags: {hashtags}")
-        memory["hashtags"] = ['Web3', 'Blockchain']
-        return memory["hashtags"]
+        try:
+            flow_id = input_data.get("flow_id")
+            steps = get_node_details(flow_id, 'Hashtag')
+            hashtags = [step.user_input for step in steps]
+            hashtags = ','.join(hashtags)
+            memory['hashtag'] = hashtags
+            return hashtags
+        except Exception as e:
+            print(f"Error Hashtag Agent: {e}")
+            return None  
